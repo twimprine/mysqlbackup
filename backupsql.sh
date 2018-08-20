05d98ba9d94042b756786d08dd149fda1f4a0e70
Thomas Wimprine <twimprine@gmail.com>
2018-08-20 09:55:25 -0500

    Create backupsql.sh

---
+++ backupsql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+DATE=`date +%m%d%Y`
+RETENTIONDAYS=28
+BACKUPDIR=/sqlbackup
+HOSTNAME=$(hostname)
+
+/usr/bin/mysqldump -uroot -pSecretP@ss! --all-databases > /${BACKUPDIR}/${HOSTNAME}_${DATE}.sql
+
+find ${BACKUPDIR} -type f -mtime +${RETENTIONDAYS} -exec rm -f {} \;
